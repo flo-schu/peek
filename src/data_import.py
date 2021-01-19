8d3cb14a1eb18061abddb874809e9071e7d31158
Florian Schunck <f.schunck@ufz.de>
2021-01-19 16:27:50 +0100

    first timeseries plot

--- src/data_import.py
+++ src/data_import.py
@@ -1,52 +1,20 @@
 from image.analysis import Data
+import evaluation.calc as calc
+from matplotlib import pyplot as plt
 
-
-d = Data("../data/annotations/", sample_id='42', date='all', img_num='all', 
+d = Data("../data/annotations/", sample_id='all', date='all', img_num='all', 
          search_keyword="motion_analysis", import_images=False,
          correct_path=(True, 3, '../data/annotations/'))
+
 d.collect()
-d.data
-len(d.data)
+dat = calc.count_organisms(d.data)
+dat = calc.id_average(dat)
 
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-df = d.data
-
-
-dat = df.groupby(['time','id']).size()
-agg = dat.groupby([pd.Grouper(freq='D', level='time'), 'id']).mean()
-
-
-ax = plt.subplot(111)
-i=1
-
-ax = ax.plot(agg.xs(3, level = "id"), 'o')
-for i in agg.index.levels[1]:
-    ax.plot(agg.xs(i, level = "id"), 'o')
-
-
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax1.plot([1,2,3],[1,2,3])
+ax = plt.subplot()
+for i in dat.index.levels[1]:
+    ax.plot(dat.xs(i, level = "id"))
 plt.show()
 
-plt.plot(agg.xs(i, level = "id"))
-
-
-
-
-from evaluation.calc import count_organisms
-dat = count_organisms(d.data)
-
-
-
-
-
-
-d.data.loc["2020-12-17"]
-
-d.data.xs(2, level = "id")
 
 # filter by date
 d.date="20201229"
--- src/evaluation/calc.py
+++ src/evaluation/calc.py
@@ -8,7 +8,7 @@ def count_organisms(df):
     """
     return df.groupby(['time','id']).size()
 
-def calculate_id_average(df, freq='D'):
+def id_average(df, freq='D'):
     """
     uses Pandas Grouper to group based on a time interval. Very useful 
     if multiple images per day have been recorded
@@ -16,3 +16,13 @@ def calculate_id_average(df, freq='D'):
     returns dataframe or series depending on input
     """
     return df.groupby([pd.Grouper(freq=freq, level='time'), 'id']).mean()
+
+
+def id_max(df, freq='D'):
+    """
+    uses Pandas Grouper to group based on a time interval. Very useful 
+    if multiple images per day have been recorded
+
+    returns dataframe or series depending on input
+    """
+    return df.groupby([pd.Grouper(freq=freq, level='time'), 'id']).max()
