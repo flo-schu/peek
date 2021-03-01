f99cb2bcc49b957962652bcc23428abef98be6fc
Florian Schunck <f.schunck@ufz.de>
2021-03-01 18:11:03 +0100

    detection with removing the median color of the search box, and then thresholding the image works actually quite well

--- src/movement_edge.py
+++ src/movement_edge.py
@@ -48,97 +48,47 @@ def filter_contours(contours):
 
     return cnts
        
-def structured_edge(roi, detector=None, thresh=0.06, morph_kernel=20):
-    edges = detector.detectEdges(roi.astype(np.float32) / 255.0)
-    ret, thresh = cv.threshold(edges,thresh,1,0)
-    morph = cv.morphologyEx(thresh, op=cv.MORPH_CLOSE, kernel=morph_kernel)
-    return [roi, edges, thresh, morph]
-
 
 m0 = Slice(img2.img)
 m1 = Slice(img1.img)
 m0.create_masks(pars="../settings/masking_20210225.json")
 m1.create_masks(pars="../settings/masking_20210225.json")
 
-i = 34
-bw = 50
-edge_detector = cv.ximgproc.createStructuredEdgeDetection('../data/dnn/model.yml')
-
-k = cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize=(40,40))
 pois = Detection.find_pois(
     m0.img, m1.img, filter_fun=filter_contours, 
     threshold=20, sw=20, erode_n=3)
 
+from skimage import measure
+roi = Detection.get_roi(m1.img, pois[51], 50)
 
-def crange(roi, smooth=3, morph_kernel=None):
-    mini = Spectral.min_filter(9, roi)
-    gray = cv.cvtColor(mini, cv.COLOR_RGB2GRAY)
-    smooth = cv.filter2D(gray, ddepth=-1, kernel=np.ones((smooth,smooth))/smooth**2)
-    horiz = cv.Sobel(smooth, 0, 1, 0, cv.CV_64F, ksize=5)
-    vert  = cv.Sobel(smooth, 0, 0, 1, cv.CV_64F, ksize=5)
-    sob = cv.bitwise_or(horiz,vert)
-    T, thresh = cv.threshold(sob, 50, 255, 0)
-    morph = cv.morphologyEx(thresh, op=cv.MORPH_CLOSE, kernel=morph_kernel)
-    return [roi, mini, gray, smooth, thresh, morph]
+gray = cv.cvtColor(roi, cv.COLOR_RGB2GRAY)
+contours = measure.find_contours(gray,60)
+contours
 
+fig, ax = plt.subplots()
+ax.imshow(gray, cmap=plt.cm.gray)
 
-def analytic(roi, detector):
-    edges = detector.detectEdges(roi.astype(np.float32) / 255.0)
-    ret, thresh = cv.threshold(edges.astype('uint8'),30,255,0)
-    print(thresh)
-    contours, h = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    img = roi.copy()
-    for i, c in enumerate(contours):
-        if cv.contourArea(c) > 10:
-            e = cv.fitEllipse(c)
-
-            img = cv.ellipse(img,e,(255,0,0),1)
-
-    return [roi, edges, thresh, img]
-
-def sobel(roi, smooth):
-    # mini = Spectral.min_filter(13, roi)
-    smooth = cv.filter2D(roi, ddepth=-1, kernel=np.ones((smooth,smooth))/smooth**2)
-    col = smooth[:,:,0] - smooth[:,:,2]
-    maxi = Spectral.max_filter(10, col)
-    horiz = cv.Sobel(maxi, 0, 1, 0, cv.CV_64F, ksize=5)
-    vert  = cv.Sobel(maxi, 0, 0, 1, cv.CV_64F, ksize=5)
-    sob = cv.bitwise_or(horiz,vert)
-    T, thresh = cv.threshold(sob, 80, 255, 0)
-
-    return [roi, smooth, col, maxi, sob]
-
-def canny(roi, smooth):
-    # mini = Spectral.min_filter(13, roi)
-    # mini = Spectral.min_filter(5, roi)
-    smooth = cv.filter2D(roi, ddepth=-1, kernel=np.ones((smooth,smooth))/smooth**2)
-    col = smooth[:,:,0] - smooth[:,:,2]
-    maxi = Spectral.max_filter(5, col)
-
-    canny = cv.Canny(maxi, 20, 40)
-
-    contours, h = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    img = roi.copy()
-    for i, c in enumerate(contours):
-        e = cv.fitEllipse(c)
-
-        img = cv.ellipse(img,e,(255,0,0),1)
-
-    return [roi, smooth, col, maxi, canny, img]
-
-crange_args = {'smooth':7, 'morph_kernel':k}
-se_args = {'detector': edge_detector, 'thresh': 0.03, 'morph_kernel': k}
-ana_args = {'detector': edge_detector}
-sobel_args = {'smooth':5}
+for n, contour in enumerate(contours):
+    ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
 
 
+def smart(roi):
+    median = cv.medianBlur(roi, 5)
+    background = median.astype('int')
+    background[:,:,2] = median[:,:,2] - np.median(median[:,:,2].flatten())
+    background[:,:,1] = median[:,:,1] - np.median(median[:,:,1].flatten())
+    background[:,:,0] = median[:,:,0] - np.median(median[:,:,0].flatten())
+    background = np.where(background > 0, background, 0).astype('uint8')
+    gray = cv.cvtColor(background, cv.COLOR_BGR2GRAY)
+    
+    T, thresh = cv.threshold(gray, 10, 255, 0)
+
+    return [roi, median, background, gray, thresh]
 
 steps = Detection.detect(
-    m1.img, pois[30], 50, 
-    structured_edge, se_args, 
+    m1.img, pois[100], 50, 
+    smart, {}, 
     plot=True)
-
-
 
 
 
