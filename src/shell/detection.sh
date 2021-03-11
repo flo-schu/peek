5a8b10ed197a2633266f2932f7b4a05b884b9f0d
flo-schu <fluncki@protonmail.com>
2021-03-11 20:46:10 +0100

    improve shell scripts pipe inputfrom ls via head to tail

--- src/shell/detection.sh
+++ src/shell/detection.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 #SBATCH -D /work/%u
-#SBATCH -J nanocosm_detect
+#SBATCH -J nanocosm_read
 #SBATCH -t 0-01:00:00
 #SBATCH --mem-per-cpu 1G 
-
 #SBATCH -o /work/%u/%x-%A-%a.out
 #SBATCH -e /work/%u/%x-%A-%a.err
 
 echo "processing chunk $SLURM_ARRAY_TASK_ID ..."
 
+INPUT_DIR=$1
+SESSION=`ls -d $INPUT_DIR*/ | head -n $SLURM_ARRAY_TASK_ID | tail -n 1`
+
+echo "processing $SESSION ..."
+
 PROJECT_DIR="/home/$USER/projects/nanocosm/"
-
-MY_INPUT_DIR=$1
-
-SERIES=`ls -d $MY_INPUT_DIR*/ | head -n $SLURM_ARRAY_TASK_ID "$MY_INPUT_DIR/dates.txt" | tail -n 1`
-
-echo "processing $SERIES ..."
-
 source "$PROJECT_DIR/env/bin/activate"
-python "/$PROJECT_DIR/src/read.py" "$SERIES"
+python "/$PROJECT_DIR/src/read.py" "$SESSION"
