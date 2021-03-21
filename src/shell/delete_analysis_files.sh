0d2ab7b82eb2e9fb847183745eaf0e29627b7c53
flo-schu <fluncki@protonmail.com>
2021-03-21 13:47:52 +0100

    additionally remove jpeg files with delete_analysis_files.sh

--- src/shell/delete_analysis_files.sh
+++ src/shell/delete_analysis_files.sh
@@ -5,6 +5,7 @@ echo $target
 
 # remove files of different types
 find $target -mindepth 1 -name "*.tiff" -print -exec rm {} \;
+find $target -mindepth 1 -name "*.jpeg" -print -exec rm {} \;
 find $target -mindepth 1 -name "*.npy" -print -exec rm {} \;
 find $target -mindepth 1 -name "*.json" -print -exec rm {} \;
 find $target -mindepth 1 -name "*.csv" -print -exec rm {} \;
