7ad8becbc792777417e99ef2dfcdf0ffa62ac413
flo-schu <fluncki@protonmail.com>
2021-03-21 14:31:44 +0100

    update shell scripts

--- src/shell/archive_qrerrors.sh
+++ src/shell/archive_qrerrors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-mkdir /work/schunckf/nanocosm/data/qr_errors/
-find /work/schunckf/nanocosm/data/pics/*/999/ -type f -name "*.jpeg" -print > /work/schunckf/nanocosm/data/qr_errors.txt
-cat /work/schunckf/nanocosm/data/qr_errors.txt | xargs cp -t /work/schunckf/nanocosm/data/qr_errors/
-tar -cvf /work/schunckf/nanocosm/data/qr_errors.tar /work/schunckf/nanocosm/data/qr_errors/
+DATE=`date +%Y%m%d`
+mkdir /work/schunckf/nanocosm/data/qr/qr_errors_$DATE/
+find /work/schunckf/nanocosm/data/pics/*/999/ -type f -name "*.jpeg" -print > /work/schunckf/nanocosm/data/qr/qr_errors_$DATE.txt
+cat /work/schunckf/nanocosm/data/qr/qr_errors_$DATE.txt | xargs cp -t /work/schunckf/nanocosm/data/qr/qr_errors_$DATE/
+tar -cvf /work/schunckf/nanocosm/data/qr/qr_errors_$DATE.tar /work/schunckf/nanocosm/data/qr/qr_errors_$DATE/
--- src/shell/copy_to_local.sh
+++ src/shell/copy_to_local.sh
@@ -2,5 +2,5 @@
 
 DATE=`date +%Y%m%d`
 echo $DATE
-scp schunckf@frontend1.eve.ufz.de:/work/schunckf/nanocosm/data/qr_errors.txt "./data/image_analysis/QR/qrcorrections/qr_errors_$DATE.txt"
-scp schunckf@frontend1.eve.ufz.de:/work/schunckf/nanocosm/data/qr_errors.tar "./data/image_analysis/QR/qr_errors_$DATE.tar"
+scp schunckf@frontend1.eve.ufz.de:/work/schunckf/nanocosm/data/qr_errors_$DATE.txt "./data/image_analysis/QR/qrcorrections/qr_errors_$DATE.txt"
+scp schunckf@frontend1.eve.ufz.de:/work/schunckf/nanocosm/data/qr_errors_$DATE.tar "./data/image_analysis/QR/qr_errors_$DATE.tar"
