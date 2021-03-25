c652d526b7107cb9641a96ec30a1a1d5aa3f2860
Florian Schunck <f.schunck@ufz.de>
2021-03-25 20:07:18 +0100

    rename files

--- src/random_forests.py
+++ src/random_forests.py
@@ -1,8 +1,11 @@
 import numpy as np
-from sklearn.datasets import load_digits
-from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+
+from sklearn.datasets import load_digits
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_predict, cross_val_score
+from sklearn import metrics as skm
 
 digits = load_digits()
 X, y = digits["data"], digits["target"]
@@ -23,28 +26,24 @@ shuffle_index = np.random.permutation(1000) # creates a random array
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 
 # binary classification
-y_train_4 = (y_train == 4) 
-y_test_4 = (y_test == 4)
 
-from sklearn.linear_model import SGDClassifier
-sgd_clf = SGDClassifier(random_state=42) # instantiate
-sgd_clf.fit(X_train, y_train_4) # train the classifier
-sgd_clf.predict([some_digit]) # make it predict some digit
+forest_clf = RandomForestClassifier(random_state=42)
+forest_clf.fit(X_train, y_train) # train the classifier
+forest_clf.predict([X[0]]) # make it predict some digit
 
 # show some results
 for index in range(0, X.shape[0], 10):
     digit = X[index]
-    predicted = sgd_clf.predict([digit])
+    predicted = forest_clf.predict([digit])
     if predicted:
         print("{} == {}".format(predicted, y[index]), end="\n")
 
+# generate cross validated predictions
+y_train_pred = cross_val_predict(forest_clf, X_train, y_train, cv=3)
+cross_val_score(forest_clf, X_train, y_train, cv=3, scoring="accuracy")
 
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, precision_recall_curve, f1_score
 
-y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_4, cv=3)
-
-confusion_matrix(y_train_4, y_train_pred, labels=[True, False])
+skm.confusion_matrix(y_train, y_train_pred, labels=[0,1,2,3,4,5,6,7,8,9])
 # interpretation
 #               predicted
 #             | 4  | Not 4 |
@@ -54,19 +53,29 @@ confusion_matrix(y_train_4, y_train_pred, labels=[True, False])
 #       --------------------
 #               99    901
 
-recall_score(y_true=y_train_4, y_pred=y_train_pred)
+# from here evaluation doesn't work anymore for multiclass
+
+
+skm.recall_score(y_true=y_train, y_pred=y_train_pred)
 # is the percentage of identified fours of all fours (97 / 98 in this case)
 # sum(y_train_4) # for checking the sum of all fours in y_train_4
 
-precision_score(y_true=y_train_4, y_pred=y_train_pred)
+skm.precision_score(y_true=y_train, y_pred=y_train_pred)
 # is correctly identified / all identified
 # if I identify 100 objects as apples but only 50 correctly i get a precision of 0.5
 
 
-f1_score(y_train_4, y_train_pred)
+skm.f1_score(y_train, y_train_pred)
 # harmonic mean from precision and recall
 
-y_scores = cross_val_predict(sgd_clf, X_train, y_train_4, cv=3, method="decision_function")
+
+# xtract y probabilities
+y_probas = cross_val_predict(forest_clf, X_train,
+                             y_train, cv=3,
+                             method="predict_proba")
+y_probas
+
+# y_scores = y_probas[:, 1] 
 
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
@@ -76,63 +85,38 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.ylim([0, 1.05])
     plt.show()
 
-precisions, recalls, thresholds = precision_recall_curve(y_train_4, y_scores)
-
-plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
-
-# now I can identify the ideal threshold value (from my decision function)
-# for optimizing recall and precision
-
-# returns the score of a digit
-sgd_clf.decision_function([some_digit])
-
-
-
-plt.plot(precisions, recalls, "b--", label="Precision")
-plt.xlabel("Precision")
-plt.ylabel("Recall")
-plt.legend(loc="upper left")
-plt.show()
-
-y_train_pred_90 = (y_scores > -600)
-precision_score(y_train_4, y_train_pred_90)
-recall_score(y_train_4, y_train_pred_90)
-
-from sklearn.metrics import roc_curve
-
-fpr, tpr, thresholds = roc_curve(y_train_4, y_scores)
-# fpr = false positive rate = (1 - true negative rate)
-# tpr = true positive rate
-
 def plot_roc_curve(fpr, tpr, label=None):
     plt.plot(fpr, tpr, linewidth=2, label=label)
     plt.plot([0, 1], [0, 1], 'k--')
     plt.axis([0, 1, 0, 1])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
+    plt.show()
     
+def plot_precision_vs_recall(precisions, recalls):
+    plt.plot(precisions, recalls, "b--", label="Precision")
+    plt.xlabel("Precision")
+    plt.ylabel("Recall")
+    plt.legend(loc="upper left")
+    plt.show()
+
+
+precisions, recalls, thresholds = skm.precision_recall_curve(y_train, y_scores)
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plot_precision_vs_recall(precisions, recalls)
+
+fpr, tpr, thresholds = skm.roc_curve(y_train_4, y_scores)
+# fpr = false positive rate = (1 - true negative rate)
+# tpr = true positive rate
+
 plot_roc_curve(fpr, tpr, "Roc Curve")
-plt.show()
-
-from sklearn.metrics import roc_auc_score
-roc_auc_score(y_train_4, y_scores)
 # area under the curve
+skm.roc_auc_score(y_train_4, y_scores)
 
-## RANDOM FORESTS
+# now I can identify the ideal threshold value (from my decision function)
+# for optimizing recall and precision
 
-from sklearn.ensemble import RandomForestClassifier
-forest_clf = RandomForestClassifier(random_state=42)
-y_probas_forest = cross_val_predict(forest_clf, X_train,
-                                    y_train_4, cv=3,
-                                    method="predict_proba")
-y_probas_forest
 
-y_scores_forest = y_probas_forest[:, 1] 
-
-fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_4, y_scores_forest)
-plot_precision_recall_vs_threshold(*precision_recall_curve(y_train_4, y_scores_forest))
-
-plt.plot(fpr, tpr, "b:", label="SGD")
-plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
-plt.legend(loc="lower right")
-plt.show()
+y_train_pred_90 = (y_scores > 0.2)
+skm.precision_score(y_train_4, y_train_pred_90)
+skm.recall_score(y_train_4, y_train_pred_90)
