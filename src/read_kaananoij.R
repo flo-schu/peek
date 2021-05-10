52db196bfebd16df220716d6f15ce62013857805
Florian Schunck <f.schunck@ufz.de>
2021-05-10 21:38:15 +0200

    avoid output of dataframes to console

--- src/read_kaananoij.R
+++ src/read_kaananoij.R
@@ -1,7 +1,7 @@
 source("../nanomodel/src/R/01_data_import.R")
 
 # import Daphnia Area
-import_nano(
+d <- import_nano(
     path = "data/pics_classic",
     fname = "Erg_gross.txt",
     n = 80,
@@ -9,7 +9,7 @@ import_nano(
     out = "data/pics_classic/results/d_size.csv")
 
 # import immobile Culex
-import_nano(
+ci <- import_nano(
     path = "data/pics_classic",
     fname = "#AbundImmo.txt",
     n = 80,
@@ -18,7 +18,7 @@ import_nano(
     out = "data/pics_classic/results/c_immo_size.csv")
 
 # import mobile Culex
-import_nano(
+cm <- import_nano(
     path = "data/pics_classic",
     fname = "#AbundMobil.txt",
     n = 80,
